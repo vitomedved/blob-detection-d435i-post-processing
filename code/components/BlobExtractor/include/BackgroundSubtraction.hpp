d033327e5ed458940618655e6be56b8112f863ed
vitomedved <vito.medved12@gmail.com>
2019-09-23 12:56:07 +0200

    Added thresholding for depth and color

---
+++ code/components/BlobExtractor/include/BackgroundSubtraction.hpp
@@ -0,0 +1,39 @@
+#ifndef BACKGROUND_SUBTRACTION_HPP
+#define BACKGROUND_SUBTRACTION_HPP
+
+#include <opencv2/opencv.hpp>
+
+
+class BackgroundSubtraction
+{
+public:
+	
+	enum SubtractorType
+	{
+		DEPTH_THRESHOLD,
+		MOG2,
+		KNN
+	};
+
+	void Subtract(cv::Mat& src, cv::Mat& dst);
+
+	void SetDepthThreshold(int threshold);
+
+	BackgroundSubtraction(BackgroundSubtraction::SubtractorType type);
+
+	BackgroundSubtraction(BackgroundSubtraction::SubtractorType type, int history, int varThreshold, bool shadows);
+
+	void Apply(cv::Mat& src, cv::Mat &dst);
+
+private:
+	bool m_backgroundSet;
+
+	int m_threshold;
+
+	SubtractorType m_type;
+
+	cv::Ptr<cv::BackgroundSubtractor> m_bgSubtractor;
+	cv::Mat m_background;
+};
+
+#endif
--- code/components/BlobExtractor/src/BlobExtractor.cpp
+++ code/components/BlobExtractor/src/BlobExtractor.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 
 #include "opencv2/opencv.hpp"
+#include "BackgroundSubtraction.hpp"
 
 void PrintHelp();
 
@@ -15,24 +16,37 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
-    char* filePath;
+    char *colorFilePath;
+    char *depthFilePath;
 
     int c;
-    while( ( c = getopt (argc, argv, "hf:") ) != -1 ) 
+    while( ( c = getopt (argc, argv, "hc:d:") ) != -1 ) 
     {
         switch(c)
         {
-            case 'f':
+            case 'c':
             {
                 if(optarg) 
                 {
-                    filePath = optarg;
+                    colorFilePath = optarg;
                 }
                 else
                 {
                     PrintHelp();
                 }
                 break;
+            }
+            case 'd':
+            {
+                if(optarg)
+                {
+                    depthFilePath = optarg;
+                }
+                else
+                {
+                    PrintHelp();
+                }
+                
             }
             case 'h':
             default:
@@ -43,29 +57,90 @@ int main(int argc, char* argv[])
         }
     }
 
-    cv::VideoCapture video(filePath);
-    if(!video.isOpened())
+    cv::VideoCapture colorVideo(colorFilePath);
+    if(!colorVideo.isOpened())
     {
-        printf("[MAIN_ERROR] Unable to open given video.\n");
+        printf("[ERROR_MAIN] Unable to open color video.\n");
+        return EXIT_FAILURE;
+    }
+
+    cv::VideoCapture depthVideo(depthFilePath);
+    if(!depthVideo.isOpened())
+    {
+        printf("[ERROR_MAIN] Unable to open depth video");
         return EXIT_FAILURE;
     }
 
     cv::namedWindow("Dummy video", cv::WINDOW_NORMAL);
 
+    BackgroundSubtraction depthSubtractor(BackgroundSubtraction::DEPTH_THRESHOLD);
+
+    cv::Ptr<cv::BackgroundSubtractorMOG2> mog2Subtractor;
+    mog2Subtractor = cv::createBackgroundSubtractorMOG2(500, 16.0, false);
+
     bool running = true;
     while(running)
     {
-        cv::Mat frame;
-        bool readFrame = video.read(frame);
-        if(readFrame)
+        cv::Mat depthFrame;
+        bool readDepthFrame = depthVideo.read(depthFrame);
+
+        cv::Mat depthOutput;
+
+        if(readDepthFrame)
         {
-            cv::imshow("Dummy video", frame);
-            cv::waitKey(0);
+            cv::inRange(depthFrame, cv::Scalar(5, 5, 5), cv::Scalar(130, 130, 130), depthOutput);
+            
+
+            uint8_t erosionDilationSize = 5;
+            cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2 * erosionDilationSize + 1, 2 * erosionDilationSize + 1), cv::Point(erosionDilationSize, erosionDilationSize));
+            cv::GaussianBlur(depthOutput, depthOutput, cv::Size(5, 5), 20);
+            cv::threshold(depthOutput, depthOutput, 150, 255, cv::THRESH_BINARY_INV);
+            cv::bitwise_not(depthOutput, depthOutput);
+            cv::dilate(depthOutput, depthOutput, kernel);
+            cv::erode(depthOutput, depthOutput, kernel);
+
+            cv::imshow("Dummy video depth", depthFrame);
+            cv::imshow("Dummy video depth mask", depthOutput);
         }
         else
         {
             printf("[MAIN_ERROR] There was an error with reading frame.\n");
+            running = false;
         }
+
+        cv::Mat colorFrame;
+        bool readColorFrame = colorVideo.read(colorFrame);
+        cv::Mat colorOutput;
+
+        if(readColorFrame)
+        {
+            mog2Subtractor->apply(colorFrame, colorOutput);
+            cv::GaussianBlur(colorOutput, colorOutput, cv::Size(5, 5), 6.0);
+            cv::threshold(colorOutput, colorOutput, 100, 255, cv::THRESH_BINARY);
+            
+            uint8_t erosionDilationSize = 4;
+            cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2 * erosionDilationSize + 1, 2 * erosionDilationSize + 1), cv::Point(erosionDilationSize, erosionDilationSize));
+            cv::dilate(depthOutput, depthOutput, kernel);
+            cv::erode(depthOutput, depthOutput, kernel);
+    
+            cv::imshow("Dummy video color frame", colorFrame);
+            cv::imshow("Dummy video bg sub", colorOutput);
+        }
+        else
+        {
+            printf("[ERROR_MAIN] There was an error with reading frame.\n");
+            running = false;
+        }
+
+        cv::Mat output;
+        if(readColorFrame && readDepthFrame)
+        {
+            cv::bitwise_and(colorOutput, depthOutput, output);
+
+            cv::imshow("Dummy video for final output", output);
+        }
+
+        cv::waitKey(100);
     }
 
     return EXIT_SUCCESS;
